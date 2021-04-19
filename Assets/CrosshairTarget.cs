c9d122aa62ca776b449d82257700b41f3fadee30
8bit <47435283+thatGuyPdeep@users.noreply.github.com>
2021-04-20 04:24:13 +0530

    Raycast
    Raycast Bullet
    Particle Effects
    Trail Renderer

---
+++ Assets/CrosshairTarget.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrosshairTarget : MonoBehaviour
+{
+    Camera mainCamera;
+    Ray ray;
+    RaycastHit hitInfo;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+        
+    }
+    void Update()
+    {
+        ray.origin = mainCamera.transform.position;
+        ray.direction = mainCamera.transform.forward;
+        Physics.Raycast(ray, out hitInfo);
+        transform.position = hitInfo.point;
+    }
+}
--- Assets/PlayerAiming.cs
+++ Assets/PlayerAiming.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 public class PlayerAiming : MonoBehaviour
 {
     public float turnSpeed = 15;
+    public float aimDuration = 0.2f;
+    public Rig aimLayer;
+    RaycastWeapon weapon;
+
     Camera mainCamera;
     // Start is called before the first frame update
     void Start()
@@ -12,13 +17,39 @@ public class PlayerAiming : MonoBehaviour
         mainCamera = Camera.main;
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        weapon = GetComponentInChildren<RaycastWeapon>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float yawCamera = mainCamera.transform.rotation.eulerAngles.y;
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yawCamera, 0), turnSpeed * Time.deltaTime);
         
     }
+
+    private void LateUpdate()
+    {
+        if (aimLayer)
+        {
+            if (Input.GetMouseButton(1))
+            {
+                aimLayer.weight += Time.deltaTime / aimDuration;
+            }
+            else
+            {
+                aimLayer.weight -= Time.deltaTime / aimDuration;
+            }
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            weapon.StartFiring();
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            weapon.StopFiring();
+        }
+    }
+
 }
